1fa6d6ceadf215a6287f692c6f823ad8a10e4d7f
Sapneswar99 <sapneswarmajhi1234@gmail.com>
2026-09-14 22:03:24 +0530

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration details and placeholders.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,16 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCFKvvStjsn1QNl56FA6vkM-xfkwWroMgQ",
+  authDomain: "sm-studio-973b7.firebaseapp.com",
+  databaseURL: "https://sm-studio-973b7-default-rtdb.firebaseio.com",
+  projectId: "sm-studio-973b7",
+  storageBucket: "sm-studio-973b7.firebasestorage.app",
+  messagingSenderId: "786740262257",
+  appId: "1:786740262257:web:5614117fa6bb63acb432d0",
+  measurementId: "G-73LT6698H7"
+};
 
+// Owner Firebase Auth UID yahan baad mein add karenge.
+const OWNER_UID = "PASTE_OWNER_FIREBASE_UID";
+
+// APK ka path
+const APK_URL = "apk/super-video-player.apk";
